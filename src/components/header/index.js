8d70d50bd9fbd7df5326d9b01936ca61e42ef997
Pete LePage <pete.lepage@pobox.com>
2021-04-30 10:01:10 -0400

    Code clean-up

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,58 +1,33 @@
-import { Component } from 'preact';
-import { Link } from 'preact-router';
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
 import style from './style.css';
 
-class Header extends Component {
-
-  shouldComponentUpdate(nextProps) {
-    const scriptID = nextProps.scriptID;
-    const selectedRoute = nextProps.selectedRoute;
-
-    this.isHome = selectedRoute === '/';
-    this.isAbout = selectedRoute === '/about';
-    this.isAccount = selectedRoute === '/account';
-    this.isEditor = selectedRoute.startsWith('/editor/');
-    this.isPrompter = selectedRoute.startsWith('/prompter/');
-
-    this.editorURL = `/editor/${scriptID}`;
-    this.prompterURL = `/prompter/${scriptID}`;
-
-    this.homeClassName = this.isHome ? style.active : '';
-    if (this.isHome || this.isAbout || this.isAccount) {
-      this.accountClassName = this.isAccount ? style.active : '';
-      this.editorClassName = style.hidden;
-      this.prompterClassName = style.hidden;
-      return true;
-    }
-    this.loginClassName = this.isLogin ? style.active : '';
-    this.editorClassName = this.isEditor ? style.active : '';
-    this.prompterClassName = this.isPrompter ? style.active : '';
-    return true;
-  }
-
-  render() {
-    return (
-      <header class={style.header}>
+const Header = ({selectedRoute, scriptID}) => {
+  return (
+    <header class={style.header}>
+      <nav>
+        <Link activeClassName={style.active} href="/">Home</Link>
+      </nav>
+      <h1>
+        MyPrompter
+      </h1>
+      { (selectedRoute?.startsWith('/editor/') || selectedRoute?.startsWith('/prompter/')) &&
         <nav>
-          <Link className={this.homeClassName} href="/">Home</Link>
-        </nav>
-        <h1>
-          MyPrompter
-        </h1>
-        <nav>
-          <Link className={this.editorClassName} href={this.editorURL}>
+          <Link activeClassName={style.active} href={`/editor/${scriptID}`}>
             Edit
           </Link>
-          <Link className={this.prompterClassName} href={this.prompterURL}>
+          <Link activeClassName={style.active} href={`/prompter/${scriptID}`}>
             Prompt
           </Link>
-          <Link className={this.accountClassName} href="/account">
-            Account
-          </Link>
         </nav>
-      </header>
-    );
-  }
+      }
+      <nav>
+        <Link activeClassName={style.active} href="/account">
+          Account
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
