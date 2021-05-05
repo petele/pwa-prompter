6cdf406309080ff62de6a7303ac9d7975f85f6b3
petele <pete.lepage@pobox.com>
2021-05-04 21:38:44 -0400

    Hosting

--- src/components/routes/home/script-list-item/index.js
+++ src/components/routes/home/script-list-item/index.js
@@ -46,13 +46,13 @@ const ScriptListItem = ({ scriptID, title, snippet, lastUpdated, hasStar, onDele
 
   return (
     <div class={style.item} href={editorURL}>
-      <button class={style.delete} type="button" onClick={clickDelete}>
+      <button class={style.delete} type="button" aria-label="delete" onClick={clickDelete}>
         <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFFFFF">
           <path d="M0 0h24v24H0V0z" fill="none" />
           <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13zM9 8h2v9H9zm4 0h2v9h-2z" />
         </svg>
       </button>
-      <button class={getStarButtonStyle()} type="button" onClick={clickStar}>
+      <button class={getStarButtonStyle()} type="button" aria-label="delete" onClick={clickStar}>
         {/* Outline */}
         <svg class={getStarStyle('outline')} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
           <path d="M0 0h24v24H0V0z" fill="none" />
