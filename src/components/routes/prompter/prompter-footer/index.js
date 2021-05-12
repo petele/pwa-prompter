4b48acabf855cdf007a0dfefb484c2de1cacd3e4
petele <pete.lepage@pobox.com>
2021-05-12 18:11:55 -0400

    Feature detect full screen

--- src/components/routes/prompter/prompter-footer/index.js
+++ src/components/routes/prompter/prompter-footer/index.js
@@ -13,6 +13,7 @@ class PrompterFooter extends Component {
     hidePlay: '',
     hidePause: style.hidden,
     fullScreenStyle: '',
+    fullScreenAvailable: document.fullscreenEnabled,
   };
 
   componentDidMount() {
@@ -223,7 +224,7 @@ class PrompterFooter extends Component {
     }
   }
 
-  render() {
+  render(props, state) {
     return (
       <footer class={style.footer} ref={el => { this._footerRef = el }}>
         <div class={style.toggle}>
@@ -258,16 +259,16 @@ class PrompterFooter extends Component {
           </button>
           <button onClick={this.toggleScroller} type="button">
             {/* PLAY */}
-            <svg class={this.state.hidePlay} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
+            <svg class={state.hidePlay} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z" />
             </svg>
             {/* PAUSE */}
-            <svg class={this.state.hidePause} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
+            <svg class={state.hidePause} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M19 19h-6V5h6v14zm-4-2h2V7h-2v10zm-4 2H5V5h6v14zm-4-2h2V7H7v10z" />
             </svg>
-            <div>{this.state.playButtonText}</div>
+            <div>{state.playButtonText}</div>
           </button>
           <button onClick={this.skipForward} type="button">
             <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
@@ -283,13 +284,15 @@ class PrompterFooter extends Component {
             </svg>
             <div>Next</div>
           </button>
-          <button id="fullScreen" class={this.state.fullScreenStyle} onClick={this.toggleFullScreen} type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
-              <path d="M0 0h24v24H0V0z" fill="none" />
-              <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
-            </svg>
-            <div>Full Screen</div>
-          </button>
+          {state.fullScreenAvailable &&
+            <button id="fullScreen" class={state.fullScreenStyle} onClick={this.toggleFullScreen} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff">
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
+              </svg>
+              <div>Full Screen</div>
+            </button>
+          }
           <button id="settings" type="button" onClick={this.onClickSettings}>
             <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#ffffff">
               <path d="M0 0h24v24H0V0z" fill="none" />
