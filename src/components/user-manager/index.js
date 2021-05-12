957f519dfc47e240bf2a9eb5ff52dd495f26b147
Pete LePage <pete.lepage@pobox.com>
2021-05-12 09:47:38 -0400

    WIP

--- src/components/user-manager/index.js
+++ src/components/user-manager/index.js
@@ -1,12 +1,11 @@
 
-import { auth } from '../firebase';
-import { sync, removeLocalData } from '../data-layer';
+import { auth, database } from '../firebase';
+
 
 export async function signIn(email, password) {
   try {
     const uCred = await auth.signInWithEmailAndPassword(email, password);
     console.log('[user_manager] Sign in succeeded', uCred.user);
-    await sync();
     return {success: true};
   } catch (ex) {
     console.warn('[user_manager] Sign in failed', ex);
@@ -21,7 +20,6 @@ export async function signIn(email, password) {
 export async function signOut() {
   try {
     await auth.signOut();
-    await removeLocalData();
     console.log('[user_manager] Sign out success.');
     return {success: true};
   } catch (ex) {
@@ -43,6 +41,14 @@ export async function createAccount(email, password, displayName) {
     console.log('[user_manager] Updated display name', displayName);
     await user.sendEmailVerification();
     console.log('[user_manager] Sent verification email.');
+    const path = `userData/${user.uid}/profile/`;
+    await database.ref(path).update({
+      createdOn: Date.now(),
+      emailAddress: email,
+      displayName,
+    });
+    console.log('[user_manager] Updated profile info.');
+    // TODO: Copy default script here.
     return {success: true};
   } catch (ex) {
     console.warn('[user_manager] Sign up failed.', ex);
@@ -57,10 +63,14 @@ export async function createAccount(email, password, displayName) {
 export async function deleteAccount(email, password) {
   try {
     const uCred = await auth.signInWithEmailAndPassword(email, password);
+    console.log('[user_manager] User re-authenticated.');
     const user = uCred.user;
-    // await removeCloudData();
-    await removeLocalData();
+    const userID = user.uid;
+    const path = `userData/${userID}`;
+    await database.ref(path).remove();
+    console.log('[user_manager] Deleted cloud data.');
     await user.delete();
+    console.log('[user_manager] User deleted.');
     return {success: true};
   } catch (ex) {
     console.warn('[account_manager] Delete account failed.', ex);
@@ -74,7 +84,7 @@ export async function deleteAccount(email, password) {
 
 export async function changePassword(email, passwordCurrent, passwordA, passwordB) {
   if (passwordA !== passwordB) {
-    console.warn('[user_manager] Password change failed. Passwords do not match!');
+    console.warn('[user_manager] Failed. Passwords do not match!');
     return {
       success: false,
       message: 'Passwords do not match.',
