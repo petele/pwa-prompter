7e0db834b4710579b40a2ae810fbac6d54dbed14
petele <pete.lepage@pobox.com>
2021-05-19 15:59:02 -0400

    login screen

--- src/components/routes/accounts/view-logged-out/index.js
+++ src/components/routes/accounts/view-logged-out/index.js
@@ -120,48 +120,61 @@ class ViewLoggedOut extends Component {
 
   render(props, state) {
     return (
-      <form class={style.signIn} onSubmit={this.submitForm}>
-        <h2>{state.title}</h2>
-        <div>
-          <label for="signin-email">Email</label>
-          <input type="email" id="signin-email" required autoComplete="email" />
-        </div>
-        {state.mode === 'sign-up' &&
-          <div>
-            <label for="signin-name">Name</label>
-            <input type="text" id="signin-name" required autoComplete="name" />
+      <div>
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active" onClick={this.showSignIn} id="nav-sign-in-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Sign In</button>
+            <button class="nav-link" onClick={this.showSignUp} id="nav-sign-up-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Sign Up</button>
+            <button class="nav-link" onClick={this.showForgotPwd} id="nav-forgot-pwa-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Forgot Password</button>
           </div>
-        }
-        {state.mode === 'sign-in' &&
-          <div>
-            <label for="signin-password">Password</label>
-            <input type="password" id="signin-password" minlength="6" required autoComplete="current-password" />
+        </nav>
+        <form class="mt-3" onSubmit={this.submitForm}>
+          {state.mode === 'sign-up' &&
+            <div class="row mb-2">
+              <label for="signin-name" class="col-3 col-form-label">Name</label>
+              <div class="col-8">
+                <input type="text" class="form-control" id="signin-name" required autoComplete="name" />
+              </div>
+            </div>
+          }
+          <div class="row mb-2">
+            <label for="signin-email" class="col-3 col-form-label">Email</label>
+            <div class="col-8">
+              <input type="email" class="form-control" id="signin-email" required autoComplete="email" />
+            </div>
           </div>
-        }
-        {state.mode === 'sign-up' &&
+          {state.mode === 'sign-in' &&
+            <div class="row mb-2">
+              <label for="signin-password" class="col-3 col-form-label">Password</label>
+              <div class="col-8">
+                <input type="password" class="form-control" id="signin-password" minlength="6" required autoComplete="current-password" />
+              </div>
+            </div>
+          }
+          {state.mode === 'sign-up' &&
+            <div class="row mb-2">
+              <label for="signin-password" class="col-3 col-form-label">Password</label>
+              <div class="col-8">
+                <input type="password" class="form-control" id="signin-password" minlength="6" required autoComplete="new-password" />
+              </div>
+            </div>
+          }
+          {state.mode === 'sign-up' &&
+            <div class="row mb-2">
+              <label for="signin-confirm" class="col-3 col-form-label">Confirm</label>
+              <div class="col-8">
+                <input type="password" class="form-control" id="signin-confirm" minlength="6" required autoComplete="new-password" />
+              </div>
+            </div>
+          }
           <div>
-            <label for="signin-password">Password</label>
-            <input type="password" id="signin-password" minlength="6" required autoComplete="new-password" />
+            <input type="submit" name="Submit" class="btn btn-primary" value={state.submitText} />
           </div>
-        }
-        {state.mode === 'sign-up' &&
-          <div>
-            <label for="signin-confirm">Confirm</label>
-            <input type="password" id="signin-confirm" minlength="6" required autoComplete="new-password" />
+          <div class={state.messageClass}>
+            {state.message}
           </div>
-        }
-        <div>
-          <input type="submit" name="Submit" value={state.submitText} />
-        </div>
-        <div class={state.messageClass}>
-          {state.message}
-        </div>
-        <div>
-          <button type="button" onClick={this.showSignIn}>Sign In</button>
-          <button type="button" onClick={this.showSignUp}>Sign Up</button>
-          <button type="button" onClick={this.showForgotPwd}>Forgot Password</button>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
